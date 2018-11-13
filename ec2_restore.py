ba51bd0f7cb4ac4f5f466859da13174546199236
0r4cl3 <lucalicheri@gmail.com>
2018-11-13 16:10:07 +0100

    add waiter on instance and deregister ami

--- ec2_restore.py
+++ ec2_restore.py
@@ -9,7 +9,9 @@ instance = ec2.Instance(instance_id)
 
 
 instance_type = instance.instance_type
-security_group = instance.security_groups[0]['GroupName']
+security_group = []
+for iterator in instance.security_groups:
+    security_group.append(iterator['GroupName'])
 
 for volume in instance.volumes.all():
     volume_size = volume.size
@@ -40,14 +42,26 @@ new_instance = ec2.create_instances(
         InstanceType=instance_type,
         MinCount=1,
         MaxCount=1,
-        SecurityGroups=[security_group],
+        SecurityGroups=security_group,
         ImageId=image_id,
+        TagSpecifications=[
+            {
+                'ResourceType': 'instance', 
+                'Tags': [
+                    {
+                        'Key': 'Name',
+                        'Value': 'New Instance automatically create'
+                        },
+                    ]
+                },
+            ],
         )
 
 print("Creating Instance")
-
+new_instance = ec2.Instance(new_instance[0].id)
+new_instance.wait_until_running()
 
 ami = ec2.Image(image_id)
 ami.deregister
 
-print("done")
+print("The new instance has been created")
